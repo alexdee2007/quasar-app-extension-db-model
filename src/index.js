0eccdde9c1894ce58f7caaf8ebb00c9e01c589af
o.diachenko.2005 <o.diachenko.2005@in.np.gov.ua>
2020-04-15 00:19:39 +0300

    add chain webpack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 /**
  * Quasar App Extension index/runner script
  * (runs on each dev/build)
@@ -8,9 +9,10 @@
 
 module.exports = function (api) {
 
-  api.compatibleWith('quasar', '^1.8.0')
-  api.compatibleWith('@quasar/app', '^1.5.0')
+  api.compatibleWith('quasar', '^1.8.0');
+  api.compatibleWith('@quasar/app', '^1.5.0');
 
-  api.resolve.app('src/db-model.js')
+  api.extendQuasarConf(conf => conf.build.transpileDependencies.push(/quasar-app-extension-db-model[\\/]src/));
+  api.chainWebpack(chain => chain.resolve.alias.set('db-model', path.resolve(__dirname, 'db-model.js')));
 
 }
